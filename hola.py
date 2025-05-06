b0c9b41a9118b6b9e76282a0b1a350c03dcdc946
m1rwebos <nhaymibarra@gmail.com>
2025-05-06 16:15:26 -0400

    repaso de prueba

--- hola.py
+++ hola.py
@@ -1,1 +1,62 @@
-print("hola mundo")
+
+
+
+
+
+
+
+# print("salio GTA6 ? 1.- si 0.- no")
+# gta6=int(input())
+
+# if gta6==1:
+#     print("se encuentra en venta")
+# else:
+#     print("se retraso ,vamooooooh")    
+
+
+
+
+
+import random
+
+print("escriba 1 numero")
+num1=int(input())
+print("escriba otro numero")
+num2=int(input())
+for i in range (num1):
+    print(random.randint(num1,num2))
+while num1 > num2:
+    print("el numero tiene que ser mayor")
+    num2
+n3=random.randint(num1,num2)
+
+for i in range (n3):  
+    print("▄"*n3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
