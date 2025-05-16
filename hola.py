99ac4949bee93f20d45887242cbea3a266b07560
m1rwebos <nhaymibarra@gmail.com>
2025-05-16 18:46:42 -0400

    waos

--- hola.py
+++ hola.py
@@ -17,36 +17,67 @@
 
 
 
-import random
+# import random
 
-print("escriba 1 numero")
-num1=int(input())
-print("escriba otro numero")
-num2=int(input())
-for i in range (num1):
-    print(random.randint(num1,num2))
-while num1 > num2:
-    print("el numero tiene que ser mayor")
-    num2
-n3=random.randint(num1,num2)
+# print("escriba 1 numero")
+# num1=int(input())
+# print("escriba otro numero")
+# num2=int(input())
+# for i in range (num1):
+#     print(random.randint(num1,num2))
+# while num1 > num2:
+#     print("el numero tiene que ser mayor")
+#     num2
+# n3=random.randint(num1,num2)
 
-for i in range (n3):  
-    print("▄"*n3)
-
-
+# for i in range (n3):  
+#     print("▄"*n3)
 
 
 
 
 
+# try:
+#     num=int(input("Ingrese un numero mayor que 3:"))
+#     if num>3:
+#        brea
+# except Exception:
+#     print("solo debe ingresar numeros")
 
 
 
 
 
+# total=0
+# carrito=0
+
+# while True: 
+#     print('''hola , eliga una opcion
+#         1.- ingrear nombre        
+#         2.- comprar,poner productos para poder comprar
+#         3.- sacar boleta,calcular pecio neto
+#         4.-salir                              
+#         ''')
+#     op=int(input(""))
+#     match  op:
+#        case 1:
+#             nombre=input("ingrese su nombre")
+#         case 2:         
 
 
 
+
+suma=0
+promedio=0
+print("ingrese cantidad de alumnos")
+alumnos=int(input())
+
+for a in range (alumnos):
+    notas=float (print(F"ingresa cuantas notas tiene el alumno", a+1))
+    
+    
+
+   
 
 
 
