2238838e10c44e94f2e0e5dcc405d693f41a6a63
m1rwebos <nhaymibarra@gmail.com>
2025-05-23 17:35:16 -0400

    saos

---
+++ #menu sushi.py
@@ -0,0 +1,73 @@
+#menu sushi
+
+# total=0
+# pikachu_roll=0
+# otaku_roll=0
+# Pulpo_venenoso_roll=0
+# Anguila_Electrica_roll=0
+# rollsTotal=0
+# while True:
+#  def Menu_Roll():
+#     print('''   
+#          elija sus opciones
+#           1.-pikachu_roll $4.500
+#           2.-otaku_roll $5.000
+#           3.-Pulpo_venenoso_roll $5.200
+#           4.-Anguila_Electrica_roll $4.800
+#           5.-salir y ver boleta
+#           ''')
+
+#     Menu_Roll()
+#     op=int(input())
+#     if op==1:
+#         print("se ha agregado un pikachu_roll ")
+#         total=total+4.500
+#         pikachu_roll=pikachu_roll+1
+#         rollsTotal=rollsTotal+1
+#         print(f"su total de rolls hasta el momento son",rollsTotal)
+#     elif op==2:
+#         print("se ha agregado un otaku_roll ")
+#         total=total+5.000
+#         otaku_roll=otaku_roll+1
+#         rollsTotal=rollsTotal+1
+#         print(f"su total de rolls hasta el momento son",rollsTotal)
+#     elif op==3:
+#         print("se ha agregado un Pulpo_venenoso_roll ")
+#         total=total+5.200
+#         Pulpo_venenoso_roll=Pulpo_venenoso_roll+1
+#         rollsTotal=rollsTotal+1
+#         print(f"su total de rolls hasta el momento son",rollsTotal)
+#     elif op==4:
+#         print("se ha agregado un  ")
+#         total=total+4.800
+#         Anguila_Electrica_roll=Anguila_Electrica_roll+1
+#         rollsTotal=rollsTotal+1
+#         print(f"su total de rolls hasta el momento son",rollsTotal)
+#     elif op==5:
+#         def bolet():
+#          print(f"el total de rolls de pikachu_roll es",pikachu_roll  )
+#          print(f"el total de rolls de otaku_roll  es",otaku_roll  )
+#          print(f"el total de rolls de Pulpo_venenoso_roll es",Pulpo_venenoso_roll  )
+#          print(f"el total de rolls de Anguila_Electrica_roll es",Anguila_Electrica_roll  )
+          
+        
+    
+total=0
+while True:
+    def Menu_rolls():
+        
+        case 
+   
+
+ 
+  
+
+
+
+
+
+
+
+
+
+
