87054e8e2ee83c533295fc0c611e0735964afdc4
m1rwebos <nhaymibarra@gmail.com>
2025-06-03 16:08:24 -0400

    tangamandapio

--- # waos.py
+++ # waos.py
@@ -17,23 +17,48 @@
 
 
 
-usuario1= None
-usuario2=None
-usuario3=None
-contraseña1= None
-contraseña2=None
-contraseña3= None
+# usuario1= None
+# usuario2=None
+# usuario3=None
+# contraseña1= None
+# contraseña2=None
+# contraseña3= None
 
 
-def inicio_sesion():
-    if usuario1==None and usuario2==None and usuario3==None:
-        print("Debe tener al menos un usuario")
-    else:
-        user=input("ingrese su usuario")
-        passw=input("ingresa su contraseña")
-        if (user==usuario1 and passw==contraseña1) or (user==usuario2 and passw==contraseña2) or (user==usuario3 and passw==contraseña3):
-            op=int(input())
+# def inicio_sesion():
+#     if usuario1==None and usuario2==None and usuario3==None:
+#         print("Debe tener al menos un usuario")
+#     else:
+#         user=input("ingrese su usuario")
+#         passw=input("ingresa su contraseña")
+#         if (user==usuario1 and passw==contraseña1) or (user==usuario2 and passw==contraseña2) or (user==usuario3 and passw==contraseña3):
+#             op=int(input())
      
+
+Productos=0
+
+
+def Menu_ese():
+ while True:
+    print('''
+     elige la wea 
+     1.- Agregar un producto
+     2.-eliminar producto
+     3.-mostrar producto
+     4.-salir
+          ''')
+op=int
+
+
+
+
+
+
+
+
+
+
+
 
 
 
